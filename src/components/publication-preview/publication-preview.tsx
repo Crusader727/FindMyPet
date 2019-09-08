1a7e281f6d28a87d1bc684d7878358c31c9221bf
Avdeev Yury <youra.avdeev@yandex.ru>
2019-09-08 19:02:07 +0300

    dirty component

--- src/components/publication-preview/publication-preview.tsx
+++ src/components/publication-preview/publication-preview.tsx
@@ -1,49 +1,76 @@
 import React, { Component, ReactNode } from "react";
 
 // Constants
-import {HIS_NAME_MIGHT_BE} from "constants/publication-preview";
+import { HIS_NAME_MIGHT_BE } from "constants/publication-preview";
 
 // Components
-import {MoreIcon} from "components/common/icons/more"
+import { MoreIcon } from "components/common/icons/more";
+import { DogIcon } from "components/common/icons/dog";
+import { LocationIcon } from "components/common/icons/location";
+import { LikedIcon } from "components/common/icons/star";
 
 // Types
-import {PublicationPreviewType} from "types/publication-preview";
+import { PublicationPreviewProps } from "types/publication-preview";
 
 import * as s from './publication-preview.scss';
 
-interface PublicationPreviewProps {
-    publicationPreview?: PublicationPreviewType;
+interface PublicationPreviewType {
+    publicationPreview?: PublicationPreviewProps;
 }
 
-const petName = "Шарик";
-// TODO массив строк с тегами
-const tag = "Есть Прививки";
-const descriptionElement = "Приучен ссать на тапки";
+export class PublicationPreview<PublicationPreviewType> extends Component {
+    static defaultProps = {
+        name: '',
+        tag: '',
+        descriptionElement: '',
+        address: '',
+        subways: [{ subway: '', color: '' }],
+    }
 
-export class PublicationPreview<PublicationPreviewProps> extends Component {
+    props: PublicationPreviewProps;
 
     public render(): ReactNode {
+        const { name, tags, descriptionElement, address, subways } = this.props;
+
         return (
             <div className={s.publication}>
                 <div className={s.publicationHeader}>
-                    <div className={s.publicationHeaderPrefixName}>
-                        {HIS_NAME_MIGHT_BE}
-                    </div>
-                    <div className={s.publicationHeaderPetName}>
-                        {petName}
+                    <DogIcon />
+                    <div className={s.publicationHeaderName}>
+                        <div className={s.publicationHeaderNamePrefixName}>
+                            {HIS_NAME_MIGHT_BE}
+                        </div>
+                        <div className={s.publicationHeaderNamePetName}>
+                            {name}
+                        </div>
                     </div>
                     <MoreIcon />
                 </div>
                 <div className={s.publicationImage} style={{ backgroundImage: `url(http://newrussianmarkets.com/wp-content/uploads/2018/10/f2d0f2696e6151d756c59f26a4bf6307.jpg)` }} />
                 <div className={s.publicationTags}>
-                    <div className={s.publicationTagsTag}>
-                        {tag}
-                    </div>
-                </div> 
+                    {tags.map((tag) =>
+                        <div className={s.publicationTagsTag}>
+                            {tag}
+                        </div>
+                    )}
+                </div>
                 <div className={s.publicationDescription}>
                     <div className={s.publicationDescriptionElement}>
                         {descriptionElement}
                     </div>
+                </div>
+                <div className={s.publicationFooter}>
+                    <LocationIcon />
+                    <div className={s.publicationFooterAddress}>
+                        {address}
+                        {subways.map((subway) =>
+                            <div className={s.publicationFooterAddressSubway}>
+                                <div style={{ backgroundColor: `${subway.color}` }} className={s.publicationFooterAddressSubwayColor}></div>
+                                {subway.subway}
+                            </div>
+                        )}
+                    </div>
+                    <LikedIcon />
                 </div>
             </div>
         );
--- src/types/publication-preview.d.ts
+++ src/types/publication-preview.d.ts
@@ -1,3 +1,9 @@
-export interface PublicationPreviewType {
-    petName?: string;
+import { string } from "prop-types";
+
+export interface PublicationPreviewProps {
+    name?: string;
+    tags?: Array<string>;
+    descriptionElement?: string;
+    address?: string;
+    subways?: Array<{ subway: string, color: string }>;
 }
